713a4d440abb0352ca84bde83442e73f3ba61e89
Zainab <zainab.kashif@outlook.com>
2026-05-12 18:26:15 +0400

    Update database password in DBConnection.java

--- ClayAndCo_New/src/main/java/DBConnection.java
+++ ClayAndCo_New/src/main/java/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL  = "jdbc:mysql://localhost:3306/claynco";
     private static final String USER = "root";         // your MySQL username
-    private static final String PASS = "imalive14"; // your MySQL password
+    private static final String PASS = "yourpassword"; // your MySQL password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
